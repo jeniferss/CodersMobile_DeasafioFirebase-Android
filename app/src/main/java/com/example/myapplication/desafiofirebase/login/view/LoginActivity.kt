95ac37fb41731b5b2ddc317721be27b3de94eb01
Jenifer Soares <jey.souza11@gmail.com>
2021-01-25 16:42:24 -0300

    Adiciona layout SaveGame

--- app/src/main/java/com/example/myapplication/desafiofirebase/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/desafiofirebase/login/view/LoginActivity.kt
@@ -1,12 +1,37 @@
 package com.example.myapplication.desafiofirebase.login.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
 import com.example.myapplication.desafiofirebase.R
+import com.example.myapplication.desafiofirebase.home.view.HomeActivity
+import com.example.myapplication.desafiofirebase.register.view.RegisterActivity
+import com.example.myapplication.desafiofirebase.savegame.view.SaveGameActivity
+import com.google.android.material.button.MaterialButton
 
 class LoginActivity : AppCompatActivity() {
+
+    private val btnCreateAccount: MaterialButton by lazy {findViewById(R.id.btnCreateAccount)}
+    private val btnLogin: MaterialButton by lazy {findViewById(R.id.btnLogin)}
+    private val checkBox: CheckBox by lazy {findViewById(R.id.cbRemember)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnCreateAccount.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, SaveGameActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
+
 }
--- app/src/main/java/com/example/myapplication/desafiofirebase/register/view/RegisterActivity.kt
+++ app/src/main/java/com/example/myapplication/desafiofirebase/register/view/RegisterActivity.kt
@@ -1,12 +1,33 @@
 package com.example.myapplication.desafiofirebase.register.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.desafiofirebase.R
+import com.example.myapplication.desafiofirebase.home.view.HomeActivity
+import com.example.myapplication.desafiofirebase.login.view.LoginActivity
+import com.google.android.material.button.MaterialButton
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val btnCreateAccount: MaterialButton by lazy {findViewById(R.id.btnCreate)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        btnCreateAccount.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
